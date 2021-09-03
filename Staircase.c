bd0fe5cfd67b7963c6fd0dae33d74e6929e2017b
Rajneesh Shukla <65530868+rajneeshshukla1608@users.noreply.github.com>
2021-09-03 22:36:55 +0530

    Update Staircase.c
    Just print the pattern - input - 6
       Output - 
          #
        ##
       ###
      ####
     #####
    ######

--- Staircase.c
+++ Staircase.c
@@ -1,1 +1,16 @@
-
+void staircase(int n) {
+    int m=1;
+for(int i=n;i>=1;i--)  
+    {  
+        for(int j=1;j<=i-1;j++)  
+        {  
+          printf(" ");  
+        }  
+        for(int k=1;k<=m;k++)  
+        {  
+            printf("#");  
+        }  
+        printf("\n");  
+        m++;  
+    }  
+}
